f7e099dbf54ef99eea9bb24f604235d2bbf2f13f
Heavyeuro <nikosbutka@gmail.com>
2019-11-21 16:21:59 +0200

    fixing loop

--- MainD.py
+++ MainD.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def convertToCSV():
-    name_CSV = 'files/csvfile.csv'
-    data_xls = pd.read_excel('files/Diabetes.xls', 'Sheet1', index_col=None)
+def convertXLSToCSV(name_CSV, name_xls):
+    name_CSV = 'files/'+name_CSV
+    data_xls = pd.read_excel('files/'+name_xls, 'Sheet1', index_col=None)
     data_xls.to_csv(name_CSV, encoding='utf-8')
     refactoringCol(name_CSV)
 
@@ -10,17 +11,21 @@ def convertToCSV():
 def refactoringCol(name_CSV):
     data_csv = pd.read_csv(name_CSV, encoding='utf-8')
 
-    i=0
-    while i < len(data_csv['Diabetes']):
+    for i in range(0, len(data_csv['Diabetes'])):
         if data_csv['Diabetes'][i] == "Healthy":
             data_csv.at[i, 'Diabetes'] = True
         else:
             data_csv.at[i, 'Diabetes'] = False
-        i = i+1
-
     data_csv.to_csv(name_CSV)
+
+def readCSV(name):
+    return pd.read_csv('files/'+name)#, index_col='Id')
 
 
 if __name__ == '__main__':
-    convertToCSV()
-    print(pd.read_csv('files/csvfile.csv', encoding='utf-8'))
+    nameXLS = 'Diabetes.xls'
+    nameCSV = 'csvData.csv'
+    convertXLSToCSV(nameCSV, nameXLS)
+
+    X_full=readCSV(nameCSV )
+    print(X_full)
